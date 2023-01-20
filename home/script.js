fc8c67bf964c5ced7862f204557901bc2d050141
hologutiworld <holoakinseloyin+github@gmail.com>
2023-01-20 23:04:03 +0100

    temporary Submission

---
+++ home/script.js
@@ -0,0 +1,110 @@
+// buttion (love-icon)//
+var count = 0;
+
+        //clothes-gallery (sweater)//
+function changeLoveIconColor1(x){
+    var x;
+    if(x == 1){
+        count = count + 1;
+    }
+
+    if(count == 1){
+        document.getElementById("love-icon-sweater").style.color="red";
+    }
+    else if(count == 2){
+        document.getElementById("love-icon-sweater").style.color="white";
+        count = 0;
+    }
+
+}
+
+    //clothes-gallery (polo)//
+function changeLoveIconColor2(x){
+    var x;
+    if(x == 1){
+        count = count + 1;
+    }
+
+    if(count == 1){
+        document.getElementById("love-icon-polo").style.color="red";
+    }
+    else if(count == 2){
+        document.getElementById("love-icon-polo").style.color="";
+        count = 0;
+    }
+
+}
+    
+        //clothes-gallery (short-black)//
+function changeLoveIconColor3(x){
+    var x;
+    if(x == 1){
+        count = count + 1;
+    }
+
+    if(count == 1){
+        document.getElementById("love-icon-black_short").style.color="red";
+    }
+    else if(count == 2){
+        document.getElementById("love-icon-black_short").style.color="white";
+        count = 0;
+    }
+
+}
+
+
+      //watch-gallery (smart-watch)//
+function changeLoveIconColor4(x){
+    var x;
+    if(x == 1){
+        count = count + 1;
+    }
+
+    if(count == 1){
+        document.getElementById("love-icon-smart_watch").style.color="red";
+    }
+    else if(count == 2){
+        document.getElementById("love-icon-smart_watch").style.color="white";
+        count = 0;
+    }
+
+}
+
+      //watch-gallery (leather-watch)//
+function changeLoveIconColor5(x){
+    var x;
+    if(x == 1){
+        count = count + 1;
+    }
+
+    if(count == 1){
+        document.getElementById("love-icon-leather_watch").style.color="red";
+    }
+    else if(count == 2){
+        document.getElementById("love-icon-leather_watch").style.color="white";
+        count = 0;
+    }
+
+}
+
+    //watch-gallery (computer-watch)//
+function changeLoveIconColor6(x){
+    var x;
+    if(x == 1){
+        count = count + 1;
+    }
+
+    if(count == 1){
+        document.getElementById("love-icon-computer_watch").style.color="red";
+    }
+    else if(count == 2){
+        document.getElementById("love-icon-computer_watch").style.color="white";
+        count = 0;
+    }
+
+}
+
+
+
+
+    
